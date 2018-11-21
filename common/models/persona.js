67b4f108842b81009b6b1ecac91220872e087443
Joaquin Diaz <joaquindiazchillagi@gmail.com>
2018-11-21 15:36:29 -0300

    integrate redis client (not working)

--- common/models/persona.js
+++ common/models/persona.js
@@ -1,6 +1,13 @@
 'use strict';
 
+var redisClient = require('redis').createClient;
+var redis = redisClient(6379, 'localhost');
+
 module.exports = function (Persona) {
+
+    var idPersona = 0;
+
+
     Persona.observe('before save', function (ctx, next) {
         if (ctx.isNewInstance !== undefined) {
             ctx.instance.fechaCreacion = new Date();
@@ -20,4 +27,22 @@ module.exports = function (Persona) {
             callback(null, promedio);
         })
     };
+
+    /*
+    Persona.on('dataSourceAttached', function () {
+
+        console.log(`CORRECAMINOS ${idPersona}`)
+        
+        var persona2ToCached = redis.get(`persona_${idPersona}`)
+        if (persona2ToCached !== undefined) {
+            var override = Persona.findById;
+            Persona.findById = function (id, callback) {
+                let result = JSON.parse(override.apply(this, arguments))
+                redis.set(`persona_${idPersona}`, result)
+                persona2ToCached = redis.get(`persona_${idPersona}`)
+            }
+        }
+        return persona2ToCached;
+    })
+    */
 };
